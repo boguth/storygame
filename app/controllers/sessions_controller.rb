56e4cfc233630e31b219110d582d9e4a2ed2b83d
boguth <bo.guthrie.mail@gmail.com>
2015-09-01 20:42:52 -0500

    In the middle of the user login.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,18 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(username: session_params[:username])
+    session[:user_id] = user.id
+    redirect_to users_path
+  end
+
+  def delete
+  end
+
+  private
+  def session_params
+    params.require(:session).permit(:username, :password)
+  end
+end
