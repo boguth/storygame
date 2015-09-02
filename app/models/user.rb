f431d86fb787480cca290c2167f3692355d646bc
boguth <bo.guthrie.mail@gmail.com>
2015-09-01 19:32:39 -0500

    got the homepage up and am working on the navbar.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ActiveRecord::Base
+  has_secure_password
+
+  validates :username, {presence: true, uniqueness: true}
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, {presence: true, uniqueness: true}
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
+  root 'users#index'
+  resources :users
+  resources :stories
+  resources :sessions, only: [:new, :create, :destroy]
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
